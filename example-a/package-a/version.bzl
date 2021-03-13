18c869d6722498527de47160bd83854b0228ea25
Roman Tsyupryk <roman.tsyupryk@asesormasmovil.es>
2021-03-13 19:20:11 +0100

    feat() refactor (main)

--- example-a/package-a/version.bzl
+++
@@ -1,8 +0,0 @@
-VERSION="v0.0.1"
-NAME="emaple-apackage-a"
-
-def get_version():
-    return VERSION
-
-def get_name():
-    return NAME
--- example-a/package-c/src/main/java/com/example/cmdline/Runner.java
+++ example-a/package-c/src/main/java/com/example/cmdline/Runner.java
@@ -1,10 +1,10 @@
 package com.example.cmdline;
 
-//import com.example.Greeting;
+import com.example.Greeting;
 
    public class Runner {
        public static void main(String args[]) {
-           System.out.println("Hello World from package-c!!!");
-//           Greeting.sayHelloWorld();
+//           System.out.println("Hello World from package-c!!!");
+           Greeting.sayHelloWorld();
    }
 }
